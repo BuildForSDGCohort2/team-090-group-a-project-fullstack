d731eff84d6df829025b6770ceaa13dc4f190ac9
HisRoyalWONGiness <talk2wong@gmail.com>
2020-09-02 20:54:11 +0100

    Work progressing on the landing page
    Added the header items, no functionality added yet.

---
+++ client/src/pages/landing/landing.styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Style = styled.div`
+
+    .oops {
+        text-align: center;
+        width: 35vw;
+        padding-top: 3em;
+        margin: auto;
+
+        @media screen and (max-width: 800px) {
+        width: 50vw;
+    }
+    }
+        
+    .comment{
+        text-align: center;
+        color: #5E5E5E;
+        width: 75%;
+        margin: auto;
+
+    }
+
+`
