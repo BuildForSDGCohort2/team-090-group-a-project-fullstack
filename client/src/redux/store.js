c6a71b1d8aa22941e4da1b49b7b2fef2b6d6dbaa
Terry Ebieto <tebieto@gmail.com>
2020-09-13 18:21:21 +0100

    added logout on user image click for debugging

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -11,9 +11,10 @@ const sagaMiddleWare = createSagaMiddleware();
 const middlewares = [sagaMiddleWare];
 
 // eslint-disable-next-line no-undef
-if(process.env.NODE_ENV === 'development') {
-	middlewares.push(logger);
-}
+// if(process.env.NODE_ENV === 'development') {
+// 	middlewares.push(logger);
+// }
+middlewares.push(logger);
 
 const store = createStore(rootReducers, applyMiddleware(...middlewares));
 
