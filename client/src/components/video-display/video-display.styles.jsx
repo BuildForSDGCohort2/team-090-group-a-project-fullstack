bfab00cae1f23ad64c7534ebe7533a418899e6b8
Terry Ebieto <tebieto@gmail.com>
2020-10-04 10:55:32 +0100

    made video height auto

--- client/src/components/video-display/video-display.styles.jsx
+++ client/src/components/video-display/video-display.styles.jsx
@@ -17,7 +17,7 @@ export const VideoDisplayContainer = styled.div`
     }
     .video-container {
         width: 100%;
-        height: 100%;
+        height: auto;
         display: flex;
         align-self: center;
     }
